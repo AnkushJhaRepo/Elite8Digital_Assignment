afb508ab9e206a012e6bb1e9f8bc1a280b868572
Ankush <ankush.work@gmail.com>
2025-07-18 01:15:59 +0530

    Update app.js to maintain CORS

--- Backend/src/app.js
+++ Backend/src/app.js
@@ -1,7 +1,12 @@
 import express from "express"
 import cors from "cors"
+import dotenv from "dotenv"
 import cookieParser from "cookie-parser"
 const app = express()
+
+dotenv.config({
+    path: "./.env"
+})
 
 app.use(
     cors({
